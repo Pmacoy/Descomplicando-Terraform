a0b1beffb550c55bf2513e2f992df4c251453567
Pmacoy <42946356+Pmacoy@users.noreply.github.com>
2024-02-13 23:38:11 +0000

    Update ec2.tf

--- Descomplicando-Terraform/ec2.tf
+++ Descomplicando-Terraform/ec2.tf
@@ -1,7 +1,7 @@
 # Create EC2 Instance - Amazon Linux
 resource "aws_instance" "web" {
   ami           = "ami-0885b1f6bd170450c"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
